d556589e733ade7d4274a81c27195d00fc6669a7
Michael Angelo De Leon <michael19972910@gmail.com>
2020-02-08 15:33:40 +0800

    .

--- lib/repository/LoginRepo.dart
+++ lib/repository/LoginRepo.dart
@@ -1,7 +1,7 @@
 import 'package:capstone/models/StudentLogin.dart';
 import 'package:capstone/provider/Login.dart';
 
-class Booking { 
+class LoginRepo { 
   final _init = Login();
 
   Future<StudentLogin> getCurrent(String user, String pwd) => _init.getStudent(user, pwd);
